12dc9d540b9ba264339b71e9385b7968e73c079c
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-06-21 14:43:41 +0530

    :)

--- 05_functions/difference.js
+++ 05_functions/difference.js
@@ -1,1 +1,17 @@
+// Change the function so that it returns the difference between number a and the sum of b and c.
 
+/*
+Sample Input 1:
+
+33 2 1
+Sample Output 1:
+
+30
+*/
+
+
+function difference(a, b, c) {
+    return a - (b + c);
+}
+
+diffrence(33, 2, 1);
