9fd3c2c017fd00d336a3a3bc77d9f07450f9693e
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-06-21 07:53:11 +0530

    :)

--- 05_functions/FunctionsEx.js
+++ 05_functions/FunctionsEx.js
@@ -1,1 +1,16 @@
+/*
+Often we have to repeat the same action in many parts of the script, for example, summing up different data entered by users each time,
+or displaying pop-ups with product descriptions for an online store.
+
+To avoid code duplication and better structure large programs, there are functions.
+
+Functions in JavaScript can be built-in or user-defined, that is, created specifically by the programmer. 
+You have already worked with a built-in function console.log(). 
+*/
+
+
+//Basic Syntax
+function name(parameters) {
+   // function body 
+}
 
