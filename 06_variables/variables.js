79264aa349b32d2484ce903699112c3b8abf2da9
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-06-21 16:04:02 +0530

    :)

--- 06_variables/variables.js
+++ 06_variables/variables.js
@@ -1,1 +1,15 @@
+//Variables declaration
+/*
+Before you start working with a variable, you need to declare (in other words, create) it. JavaScript uses two keywords to create variables:
+
+let defines a mutable variable the value of which can be changed as many times as needed;
+const declares a constant whose value you want to forbid overwriting.
+When you declare a variable, you must give it a name after one of these keywords. 
+It is good practice to assign a variable name that describes its contents.
+Always try to choose meaningful and readable names for variables to make your code easy to understand.
+
+*/
+
+
+// Mutable variables
 
