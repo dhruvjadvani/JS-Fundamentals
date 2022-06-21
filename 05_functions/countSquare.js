c71719718127689650bf95d84b7793e375f1f0cd
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-06-21 14:41:22 +0530

    :)

--- 05_functions/countSquare.js
+++ 05_functions/countSquare.js
@@ -1,1 +1,9 @@
+// Declare a function named countSquare that takes a number and returns the square of it.
+
+// write your function here
+function countSquare(num) {
+  return num * num;
+}
+
+countSquare(2);
 
