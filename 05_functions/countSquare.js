f88dbf3e8d7ab73269342b3476c32c47da0dfd7f
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-19 20:34:12 +0530

    Delete countSquare.js

--- 05_functions/countSquare.js
+++
@@ -1,9 +0,0 @@
-// Declare a function named countSquare that takes a number and returns the square of it.
-
-// write your function here
-function countSquare(num) {
-  return num * num;
-}
-
-countSquare(2);
-
