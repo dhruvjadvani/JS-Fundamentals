e42c2b850a65b91ff09cb909feee99eeb0f68fd0
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-21 14:27:10 +0530

    Update ex1.js

--- 10_Functions/ex1.js
+++ 10_Functions/ex1.js
@@ -1,1 +1,8 @@
+// Declare a function named countSquare that takes a number and returns the square of it.
 
+// write your function here
+function countSquare(a) {
+  return a * a;
+}
+
+countSquare(2);
