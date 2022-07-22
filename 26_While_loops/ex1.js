3c095c59ee069137468c64ff8614abca39234347
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-23 02:19:01 +0530

    Update ex1.js

--- 26_While_loops/ex1.js
+++ 26_While_loops/ex1.js
@@ -1,1 +1,15 @@
+/*
+Imagine that you are a bus driver. You stop at a traffic light and see your friend Harry crossing the road.
+While the light is red, you have a chance to say hello to your friend, and you yell: "Hey Harry!". Once you have greeted each other,
+the light turns green and you have to keep driving.
 
+Interpret this scenario using the while loop. Create a variable isRed and set the initial value to true.
+Display the message Hey Harry! inside the loop and then, when the light goes green, change the value of the isRed variable.
+*/
+
+let isRed = true;
+
+while(isRed) {
+  console.log("Hey Harry!");
+  isRed = false;
+}
