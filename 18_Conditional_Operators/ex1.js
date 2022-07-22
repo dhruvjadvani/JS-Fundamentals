ffe82152d05674815a47201482fdb0d9912603da
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-22 17:57:27 +0530

    Update ex1.js

--- 18_Conditional_Operators/ex1.js
+++ 18_Conditional_Operators/ex1.js
@@ -1,1 +1,14 @@
+/*
+Imagine you want to go to the pub this evening, but you look too young. 
+The pub staff knows nothing about your real age, so now you need to implement the age checking procedure.
+
+Set the value of 18 to the variable age. Set the condition of age checking to the variable condition: is your age less than 18 or not? 
+If so, you need to write in the console "Sorry, but you are a minor.". If the age is more than or equal to 18, the message is "That's ok. You can enter the pub.".
+*/
+
+let age  = 18;
+
+let condition = age < 18 ? "Sorry, but you are a minor." : "That's ok. You can enter the pub.";
+
+console.log(condition);
 
