8cb582c112f230e72f43cde8064748a7ebda171a
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-23 00:52:03 +0530

    Update ex2.js

--- 23_Arrow_Functions/ex2.js
+++ 23_Arrow_Functions/ex2.js
@@ -1,1 +1,6 @@
+// Declare a function named multiplication, which takes any two numbers as parameters and returns the result of their multiplication.
 
+// write your code here
+let multiplication = (a, b) => {
+  return a * b;
+};
