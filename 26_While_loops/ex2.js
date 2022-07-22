2747ec9efb69b0512e362c37f3827ce0401241ea
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-23 02:22:40 +0530

    Update ex2.js

--- 26_While_loops/ex2.js
+++ 26_While_loops/ex2.js
@@ -1,1 +1,18 @@
+/*
+Assume you are taking a glass elevator to the 10th floor of a business center.
+While below the 6th floor, all you see is twigs of the trees growing near the center. 
+Once you get higher though, you get a wonderful view of the city.
 
+Set the variable n = 1 which will increase the number of the floor you've reached consequently, one by one. 
+Check if that number is not greater than 10 in the while loop. If you are lower than the 6th floor, display Tree!, otherwise, display City!
+*/
+
+let n = 1;
+while(n <= 10) {
+  if(n < 6) {
+    console.log("Tree!");
+  } else {
+    console.log("City!");
+  }
+  n++;
+}
