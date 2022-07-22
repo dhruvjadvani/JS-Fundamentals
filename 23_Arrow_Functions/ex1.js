f5d29ff9c89c1861b09b000addb5f9137734eb84
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-23 00:49:18 +0530

    Update ex1.js

--- 23_Arrow_Functions/ex1.js
+++ 23_Arrow_Functions/ex1.js
@@ -1,1 +1,12 @@
+/*
+Define a function that will add the word "captain" before the name of a person. There should be a space after the word "captain".
 
+The function should be named captainAdder and accept one parameter name as an argument.
+
+The function should return a string, not print it to the console.
+*/
+
+// write your code here
+let captainAdder = (name) => {
+  return `captain ${name}`;
+  };
