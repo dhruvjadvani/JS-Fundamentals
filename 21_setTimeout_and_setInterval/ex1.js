b3a9826805713df5dd4a77e9c8d942b040fe64d6
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-22 23:34:52 +0530

    Update ex1.js

--- 21_setTimeout_and_setInterval/ex1.js
+++ 21_setTimeout_and_setInterval/ex1.js
@@ -1,1 +1,34 @@
 
+// What will be the output of the following code snippet? Print the values in one row without spaces in between.
+
+console.log(5);
+
+function printOne() {
+ console.log(1);
+}
+
+console.log(9);
+
+function printFour() {
+ console.log(4);
+}
+
+setTimeout(printSix, 3000)
+setTimeout(printOne, 0)
+setTimeout(printFour, 0)
+setTimeout(printSeven, 2000)
+
+function printSix() {
+ console.log(6);
+}
+
+function printSeven() {
+ console.log(7);
+}
+
+//5
+//9
+//1
+//4
+//7
+//6
