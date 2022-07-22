ac22ab838f2e2093687eecd24e03f3221220f2a7
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-22 10:30:01 +0530

    Update ex2.js

--- 13_JSON/ex2.js
+++ 13_JSON/ex2.js
@@ -1,1 +1,22 @@
+// Create a JSON object that represents a library.
 
+// A library has only a single books field, but each book has exactly two fields: title and author. Assign information about books you've read to these fields.
+
+// Fill the library with at least three books and enter the resulting JSON.
+
+{
+  "Books": [
+    {
+      "title": "Rich Dad Poor Dad",
+      "author": "Robert Kiyosaki"
+    },
+    {
+      "title": "Deep work",
+      "author": "Carl Newport"
+    },
+    {
+      "title": "Atomic Habits",
+      "author": "James Clear"
+    }
+  ]
+}
