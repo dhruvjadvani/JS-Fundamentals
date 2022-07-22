72ae7c4c83f1ac984c77b9d65b3683d52f35a509
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-22 18:00:57 +0530

    :)

--- 18_Conditional_Operators/ex2.js
+++ 18_Conditional_Operators/ex2.js
@@ -1,1 +1,14 @@
+/*
+What do you know about the speed of light and the speed of sound? The speed of light is 299792458 m/s. 
+Set the speed of light to the variable light, and the speed of sound (343 m/s) to the variable sound.
 
+Using the ternary operator, set the variable lightIsPower true if the speed of light is greater than the speed of sound and false otherwise. 
+Print the variable result in the console.
+*/
+
+let light = 299792458;
+let sound = 343;
+
+let lightIsPower = light > sound ? true : false;
+
+console.log(lightIsPower);
