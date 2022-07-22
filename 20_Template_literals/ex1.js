bc2e8ed2e19b4c055e8a25e0c685363537183852
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-22 23:08:45 +0530

    Update ex1.js

--- 20_Template_literals/ex1.js
+++ 20_Template_literals/ex1.js
@@ -1,1 +1,8 @@
+// Using template literals, write the code so that the result of its execution is the output of the following string to the console:
 
+// Brad took first place in the game.
+
+let userName = "Brad";
+let place = "first";
+// write your code below
+console.log(`${userName} took ${place} place in the game.`);
