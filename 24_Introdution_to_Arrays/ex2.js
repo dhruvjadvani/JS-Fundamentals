366004440e502d8c06b64f90de3be8ab4444cda9
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-23 01:16:19 +0530

    Update ex2.js

--- 24_Introdution_to_Arrays/ex2.js
+++ 24_Introdution_to_Arrays/ex2.js
@@ -1,1 +1,10 @@
+/*
+Finish writing the checkData function. It should accept a data structure and check whether it's an Array or not.
+
+Use console.log() to print the result.
+*/
+
+function checkData(data) {
+  console.log(Array.isArray(data));
+}
 
