b84ef3eaa7448b2cea91039a93744f8100f9b149
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-22 17:39:44 +0530

    Update ex1.js

--- 17_Comparison_Operators/ex1.js
+++ 17_Comparison_Operators/ex1.js
@@ -1,1 +1,8 @@
+// Write and call a function that displays the result of a strict comparison of number 5 and string "5" in the console.
 
+// Write your code here
+function compare(a, b) {
+  console.log(a === b);
+}
+
+compare(5, "5"); // false
