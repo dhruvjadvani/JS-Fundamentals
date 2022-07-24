a20d55005b010c9273bdeb3d43759292b90dd5d2
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-24 22:29:32 +0530

    Update ex1.js

--- 29_For_Loops/ex1.js
+++ 29_For_Loops/ex1.js
@@ -1,1 +1,16 @@
+/*
+Write a program that calculates the sum of all numbers from 1 to 100 inclusive using loops and displays the result to the console.
+
+Don't forget to call the function.
+*/
+
+function sum (a, b) {
+  let v = 0;
+  for(let i = a; i <= b; i++) {
+    v = v + i;
+  }
+  return v;
+}
+
+console.log(sum(1, 100));
 
