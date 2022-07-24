68360e34154f17833e448f0b525038a96dbbe510
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-24 22:32:24 +0530

    Update ex2.js

--- 29_For_Loops/ex2.js
+++ 29_For_Loops/ex2.js
@@ -1,1 +1,7 @@
+// Create an array which contains the following list of fruits: apple, orange, banana. Write a program that will display in the console these fruits line by line.
 
+let fruits = ['apple', 'orange', 'banana'];
+
+for(i in fruits) {
+  console.log(fruits[i]);
+}
