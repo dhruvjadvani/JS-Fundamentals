082412c5d7a526b145c9ee58d0d9e8369f4b90a0
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-24 22:49:56 +0530

    Update ex1.js

--- 30_Object_methods_and_keyword "this"/ex1.js
+++ 30_Object_methods_and_keyword "this"/ex1.js
@@ -1,1 +1,16 @@
+/*
+Create a box object that has three numeric properties to store its measurements:
+length, width, and height. Assign the values 20, 30, and 15 to these properties accordingly.
 
+In addition, you need to create getVolume function, which should return the volume of the box, as the fourth property of the object.
+*/
+
+// write your object here
+let box = {
+  length: 20,
+  width: 30,
+  height: 15,
+  getVolume() {
+    return this.length * this.width * this.height;
+  }
+};
