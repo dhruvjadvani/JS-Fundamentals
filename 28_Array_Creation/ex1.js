5a9f8d1f6c8c83e522a4ee95833dc62144bd2a50
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-24 22:15:06 +0530

    Update ex1.js

--- 28_Array_Creation/ex1.js
+++ 28_Array_Creation/ex1.js
@@ -1,1 +1,8 @@
+/*
+In the code below create an array named threeArray with five positions and fill all the positions with the number 3 using the fill() method.
 
+In the end just print the array.
+*/
+
+let threeArray = new Array(5).fill(3);
+console.log(threeArray);
