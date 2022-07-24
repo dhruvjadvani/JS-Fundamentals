b9c1c71230aa64e12e8176d47d8180fae5ed8ffc
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-24 22:53:05 +0530

    Update ex2.js

--- 30_Object_methods_and_keyword "this"/ex2.js
+++ 30_Object_methods_and_keyword "this"/ex2.js
@@ -1,1 +1,16 @@
+/*
+Create a counter object that has the following properties:
 
+current with a value of 0;
+the inc function, which adds 1 to the current.
+The function should return the value, not print it.
+
+*/
+
+// write your object here
+let counter = {
+  current: 0,
+  inc() {
+    return this.current + 1;
+  }
+};
