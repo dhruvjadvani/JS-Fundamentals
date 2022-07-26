3c790ad9913171c6a8fb29a7eecffff77283bec6
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-27 02:22:10 +0530

    Update ex2.js

--- 36_Basic_string_methods/ex2.js
+++ 36_Basic_string_methods/ex2.js
@@ -1,1 +1,17 @@
+/*
+You need to use an appropriate JavaScript method on the string below so that the output of the below code will be the following: 
 
+['hello@yoursite', 'edu', 'co', 'mak'].
+
+Sample Input 1:
+hello@yoursite.edu.co.mak
+
+Sample Output 1:
+[ 'hello@yoursite', 'edu', 'co', 'mak' ]
+*/
+
+
+// Please don't modify the code below.
+const string = input;
+// Write your code below this line.
+console.log(string.split("."));
