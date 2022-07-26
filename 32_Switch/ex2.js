7a288ae977d46a6554071dce7bc84d18ddcfc3f1
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-26 19:15:08 +0530

    Update ex2.js

--- 32_Switch/ex2.js
+++ 32_Switch/ex2.js
@@ -1,1 +1,31 @@
 
+// Rewrite the languageInfo function using the switch operator.
+
+function languageInfo(language) {
+  if (language === "JavaScript"){
+    console.log("Begins on December 4, 1995");
+  } else if (language === "Java") {
+    console.log("Begins on May 23, 1995");
+  } else if (language === "PHP") {
+    console.log("Begins on June 8, 1995");
+  }
+}
+
+
+
+
+// ans
+function languageInfo(language){
+     // write your code here
+     switch(language) {
+          case "JavaScript":
+            console.log("Begins on December 4, 1995");
+            break;
+          case "Java":
+            console.log("Begins on May 23, 1995");
+            break;
+          case "PHP":
+            console.log("Begins on June 8, 1995");
+            break;
+     }
+}
