657c503e8dd4d514bed06e607e78d23e7052fdfd
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-26 21:04:16 +0530

    Update ex1.js

--- 33_Break_and_Continue/ex1.js
+++ 33_Break_and_Continue/ex1.js
@@ -1,1 +1,20 @@
+/*
+You are given an array of numbers called numbers.
+Write a cycle that logs "even" for each even number and "odd" for odd numbers. If the number is 0, the program must stop processing the numbers.
+
+For the number 0, the program must not print any message.
+*/
+
+function checkEvenOrOdd(numbers) {
+    // write your code here
+    for(let i = 0; i < numbers.length; i++){
+        if (numbers[i] === 0) {
+            break;
+        } else if (numbers[i] % 2 === 0) {
+            console.log("even");
+        } else if (numbers[i] % 2 !== 0) {
+            console.log("odd");
+        }
+    }
+}
 
