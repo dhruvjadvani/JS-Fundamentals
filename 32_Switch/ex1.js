d27a0b6f2de63cee1c33b73451baa87c43cb9bea
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-26 19:10:30 +0530

    Update ex1.js

--- 32_Switch/ex1.js
+++ 32_Switch/ex1.js
@@ -1,1 +1,38 @@
+/*
+Create a switch operator inside the function getSalary. It should show the following text in the console.log depending on the values:
 
+value	          text in console.log
+"Bootstrap"         $15 per hour
+
+"Chrome Extension"  $20 per hour
+
+"React"             $30 per hour
+
+other cases         $25 per hour
+
+
+Sample Input 1:
+Bootstrap
+
+Sample Output 1:
+$15 per hour
+
+*/
+
+function getSalary(value){
+     // write your code here
+     switch(value) {
+          case "Bootstrap":
+            console.log("$15 per hour");  
+            break;
+          case "Chrome Extension":
+            console.log("$20 per hour");
+            break;
+          case "React":
+            console.log("$30 per hour");
+            break;
+          default:
+            console.log("$25 per hour");
+            break;
+     }
+}
