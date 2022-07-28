76c3b1d6809f805bbf752fc58f1f023c816d8446
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-28 11:22:27 +0530

    Update ex1.js

--- 41_Scope_of_variables/ex1.js
+++ 41_Scope_of_variables/ex1.js
@@ -1,1 +1,15 @@
+/*
+Write a function myFunc() where you create a counter variable i and use it inside the for loop. In the loop, print out all the cubes of numbers from 1 to 5.
+
+Think about which kind of identifier is more appropriate here.
+
+Also, make sure your code defines the function but doesn't call it.
+*/
+
+function myFunc() {
+  for(let i = 1; i <= 5; i++) {
+    console.log(i * i * i);
+  }
+}
+
 
